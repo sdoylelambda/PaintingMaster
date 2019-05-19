78c3d06111a43ed5ca2b15d231171cd9314a2188
Sean Doyle <sdoylelambda@gmail.com>
2019-05-19 13:03:39 -0400

    All requirements met.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,19 @@ class Panel{
   panels.forEach( function(panel){
 	return new Panel(panel);
   });
+
+
+
+  // Email send button
+  function sendMail() {
+    var link = "mailto:me@example.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
   
 
 // end of my code
